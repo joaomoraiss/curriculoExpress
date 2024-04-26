5f1faf879a2f6ccc697daf4503b65ba0b8422237
joaomoraiss <joaovictorrmpework@outlook.com>
2024-04-26 11:05:43 -0300

    criação pasta

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db');
-const routes = require('./routes');
+const db = require('../db');
+const routes = require('../routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
